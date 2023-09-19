664f74670a3f4fddeee5a65f2a761ea0fdb81025
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-09-19 22:49:32 +0800

    Add user login

---
+++ api/src/services/handlers/login_handler.ts
@@ -0,0 +1,176 @@
+/**
+ * @file Defines {@link LoginHandler}.
+ */
+import bcrypt from 'bcrypt';
+import express from 'express';
+import pg from 'pg';
+import qs from 'qs';
+import { v4 as uuidV4 } from 'uuid';
+
+import HttpInfoError from '../../errors/http_info_error';
+import { parsePassword, parseUsername } from '../../utils/data_parser';
+import {
+  createUserSessionEntry,
+  fetchPasswordHashFromUsername,
+  isDuplicateUserSessionTokenError,
+} from '../../utils/database_util';
+import Handler, { HttpMethod } from './handler';
+
+/** Handles user login. */
+export default class LoginHandler implements Handler {
+  private readonly _sessionExpireMillis: number;
+
+  public constructor(sessionExpireMillis: number) {
+    this._sessionExpireMillis = sessionExpireMillis;
+  }
+
+  public get method(): HttpMethod {
+    return HttpMethod.post;
+  }
+
+  public get path(): string {
+    return '/login';
+  }
+
+  private static _parseQuery(query: qs.ParsedQs): [string, string] {
+    try {
+      const username = parseUsername(query['username']);
+      const password = parsePassword(query['password']);
+
+      return [username, password];
+    } catch (e) {
+      throw new HttpInfoError(400, (e as Error).message);
+    }
+  }
+
+  private static async _authenticate(
+    client: pg.ClientBase,
+    username: string,
+    password: string,
+    sessionExpireMillis: number,
+  ): Promise<[string, Date]> {
+    await LoginHandler._verifyIdentity(client, username, password);
+    return await LoginHandler._createSession(
+      client,
+      username,
+      sessionExpireMillis,
+    );
+  }
+
+  private static async _verifyIdentity(
+    client: pg.ClientBase,
+    username: string,
+    password: string,
+  ): Promise<void> {
+    const passwordHash: string = await LoginHandler._fetchPasswordHash(
+      client,
+      username,
+    );
+
+    if (!(await LoginHandler._doesPasswordMatch(password, passwordHash))) {
+      throw new HttpInfoError(401);
+    }
+  }
+
+  private static async _fetchPasswordHash(
+    client: pg.ClientBase,
+    username: string,
+  ): Promise<string> {
+    const passwordHash: string | undefined =
+      await fetchPasswordHashFromUsername(client, username);
+    if (passwordHash === undefined) {
+      throw new HttpInfoError(401);
+    }
+
+    return passwordHash;
+  }
+
+  private static async _doesPasswordMatch(
+    password: string,
+    passwordHash: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, passwordHash);
+  }
+
+  private static async _createSession(
+    client: pg.ClientBase,
+    username: string,
+    sessionExpireMillis: number,
+  ): Promise<[string, Date]> {
+    const loginTime: Date = new Date(Date.now());
+    const expireTime: Date = new Date(Date.now() + sessionExpireMillis);
+
+    let token: string = '';
+
+    let isEntryCreated: boolean = false;
+    while (!isEntryCreated) {
+      token = uuidV4();
+
+      try {
+        await createUserSessionEntry(
+          client,
+          token,
+          username,
+          loginTime,
+          expireTime,
+        );
+
+        isEntryCreated = true;
+      } catch (e) {
+        if (!isDuplicateUserSessionTokenError(e)) {
+          throw e;
+        }
+      }
+    }
+
+    return [token, expireTime];
+  }
+
+  /**
+   * Creates a new user session in the database if the username and password
+   * provided in the request are a match. Sends a HTTP 200 response.
+   *
+   * If the username and/or password are invalid (missing, empty etc.), sends
+   * a HTTP 400 response containing the reason for the error in the response
+   * message.
+   *
+   * If no user with the username exist, or the username and password do not
+   * match, sends a HTTP 401 response.
+   *
+   * If an internal server error occurs, sends a HTTP 500 response.
+   * @param req - Information about the request.
+   * @param res - For creating and sending the response.
+   * @param next - Called to let the next handler (if any) handle the request.
+   * @param client - Client for communicating with the database.
+   */
+  public async handle(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+    client: pg.ClientBase,
+  ): Promise<void> {
+    try {
+      const [username, password]: [string, string] = LoginHandler._parseQuery(
+        req.query,
+      );
+
+      const sessionToken: [string, Date] = await LoginHandler._authenticate(
+        client,
+        username,
+        password,
+        this._sessionExpireMillis,
+      );
+
+      res
+        .status(200)
+        .cookie('session_token', sessionToken[0], { expires: sessionToken[1] })
+        .send();
+    } catch (e) {
+      if (e instanceof HttpInfoError) {
+        res.status(e.statusCode).send(e.message);
+      } else {
+        res.sendStatus(500);
+      }
+    }
+  }
+}
--- api/src/utils/database_util.ts
+++ api/src/utils/database_util.ts
@@ -6,6 +6,30 @@ import pg from 'pg';
 import UserProfile from '../data/user_profile';
 
 /**
+ * Fetches the password hash of the user whose username is {@link username}.
+ * @param client - Client for communicating with the database.
+ * @param username - Username of the user.
+ * @returns Password hash of the user if a user with the username exist. Else,
+ * returns undefined.
+ */
+export async function fetchPasswordHashFromUsername(
+  client: pg.ClientBase,
+  username: string,
+): Promise<string | undefined> {
+  const result: pg.QueryResult = await client.query(
+    'SELECT password_hash FROM User_Credentials WHERE user_id IN (' +
+      '  SELECT user_id FROM User_Profiles WHERE username=$1)',
+    [username],
+  );
+
+  if (result.rows.length == 0) {
+    return undefined;
+  }
+
+  return result.rows[0]['password_hash'];
+}
+
+/**
  * Creates an entry in the database for a user profile and another entry for a
  * user credential corresponding to the user profile.
  * @param client - Client for communicating with the database.
@@ -23,6 +47,28 @@ export async function createUserProfileAndCredentialEntry(
       '  VALUES ($1, $2) RETURNING user_id) ' +
       'INSERT INTO User_Credentials SELECT user_id, $3 FROM user_id_cte;',
     [userProfile.username, userProfile.email, passwordHash],
+  );
+}
+
+/**
+ * Creates an entry in the database for a user session.
+ * @param client - Client for communicating with the database.
+ * @param token - Session token.
+ * @param username - Username of the user.
+ * @param loginTime - Time of session creation.
+ * @param expireTime - Time of session expiry.
+ */
+export async function createUserSessionEntry(
+  client: pg.ClientBase,
+  token: string,
+  username: string,
+  loginTime: Date,
+  expireTime: Date,
+): Promise<void> {
+  await client.query(
+    'INSERT INTO User_Sessions (token, user_id, expire_time) ' +
+      '  SELECT $1, user_id, $2 FROM User_Profiles WHERE username=$3',
+    [token, expireTime, username],
   );
 }
 
@@ -50,6 +96,20 @@ export function isDuplicateUserProfileEmailError(err: unknown): boolean {
     err instanceof Error &&
     err.message.includes(
       'duplicate key value violates unique constraint "user_profiles_email_key"',
+    )
+  );
+}
+
+/**
+ * @param err - The error to check.
+ * @returns True if {@link err} is an {@link Error} caused by a duplicated
+ * session token in the user_sessions database relation.
+ */
+export function isDuplicateUserSessionTokenError(err: unknown): boolean {
+  return (
+    err instanceof Error &&
+    err.message.includes(
+      'duplicate key value violates unique constraint "user_sessions_pkey"',
     )
   );
 }
