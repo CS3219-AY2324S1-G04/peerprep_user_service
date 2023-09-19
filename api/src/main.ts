3a13301b579dfcc000abb4ed383e4dfaf08037df
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-09-19 22:51:44 +0800

    Add app entrypoint

---
+++ api/src/main.ts
@@ -0,0 +1,36 @@
+/**
+ * @file Entrypoint to the app.
+ */
+import pg from 'pg';
+
+import Config from './data/config';
+import App from './services/app';
+import DeleteUserHandler from './services/handlers/delete_user_handler';
+import GetUserProfileHandler from './services/handlers/get_user_profile_handler';
+import LoginHandler from './services/handlers/login_handler';
+import RegisterHandler from './services/handlers/register_handler';
+import UpdateUserProfileHandler from './services/handlers/update_user_profile_handler';
+
+const config: Config = new Config();
+const pgPool: pg.Pool = new pg.Pool({
+  password: config.pgPassword,
+  user: config.pgUser,
+  host: config.pgHost,
+  port: config.pgPort,
+  database: config.pgDatabase,
+  connectionTimeoutMillis: config.pgPoolConnectionTimeoutMillis,
+  idleTimeoutMillis: config.pgPoolIdleTimeoutMillis,
+});
+
+// TODO: Admin account
+// TODO: Change password
+// TODO: Logout
+const app: App = new App(config.port, pgPool, [
+  new RegisterHandler(config.hashCost),
+  new LoginHandler(config.sessionExpireMillis),
+  new GetUserProfileHandler(),
+  new UpdateUserProfileHandler(),
+  new DeleteUserHandler(),
+]);
+
+app.start();
