bec232b53cf66f26bff972e05706eb3c2c8943b9
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-10-17 23:28:32 +0800

    Add password validation when deleting user

--- api/src/handlers/delete_user_handler.ts
+++ api/src/handlers/delete_user_handler.ts
@@ -1,13 +1,15 @@
 /**
  * @file Defines {@link DeleteUserHandler}.
  */
+import bcrypt from 'bcrypt';
 import express from 'express';
 
 import HttpErrorInfo from '../data_structs/http_error_info';
 import SessionToken from '../data_structs/session_token';
 import DatabaseClient from '../service/database_client';
 import Handler, { HttpMethod } from './handler';
-import { sessionTokenKey } from '../utils/parameter_keys';
+import { passwordKey, sessionTokenKey } from '../utils/parameter_keys';
+import Password from '../data_structs/password';
 
 /** Handles deleting the user who sent the request. */
 export default class DeleteUserHandler extends Handler {
@@ -19,6 +21,18 @@ export default class DeleteUserHandler extends Handler {
     return '/user-service/user';
   }
 
+  private static _parseQuery(query: qs.ParsedQs): Password {
+    let password: Password;
+
+    try {
+      password = Password.parse(query[passwordKey]);
+    } catch (e) {
+      throw new HttpErrorInfo(401);
+    }
+
+    return password!;
+  }
+
   private static _parseCookie(cookies: {
     [x: string]: string | undefined;
   }): SessionToken {
@@ -27,6 +41,42 @@ export default class DeleteUserHandler extends Handler {
     } catch (e) {
       throw new HttpErrorInfo(401);
     }
+  }
+
+  private static async _validatePassword(
+    client: DatabaseClient,
+    sessionToken: SessionToken,
+    password: Password,
+  ): Promise<void> {
+    const passwordHash: string = await DeleteUserHandler._fetchPasswordHash(
+      client,
+      sessionToken,
+    );
+
+    if (!(await DeleteUserHandler._doesPasswordMatch(password, passwordHash))) {
+      throw new HttpErrorInfo(401);
+    }
+  }
+
+  private static async _fetchPasswordHash(
+    client: DatabaseClient,
+    sessionToken: SessionToken,
+  ): Promise<string> {
+    const passwordHash: string | undefined =
+      await client.fetchPasswordHashFromSessionToken(sessionToken);
+
+    if (passwordHash === undefined) {
+      throw new HttpErrorInfo(401);
+    }
+
+    return passwordHash;
+  }
+
+  private static async _doesPasswordMatch(
+    password: Password,
+    passwordHash: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password.toString(), passwordHash);
   }
 
   private static async _deleteUserProfile(
@@ -40,12 +90,14 @@ export default class DeleteUserHandler extends Handler {
 
   /**
    * Deletes the user who owns the session token stored in the request cookie.
+   * Sends a HTTP 200 response.
    * @param req - Information about the request. Sends a HTTP 200 response.
    * @param res - For creating and sending the response.
    * @param next - Called to let the next handler (if any) handle the request.
    * @param client - Client for communicating with the database.
-   * @throws {HttpErrorInfo} Error 401 if no session token is found or the
-   * session token is invalid (expired or not owned by any user).
+   * @throws {HttpErrorInfo} Error 401 if no session token is found, or the
+   * session token is invalid (expired or not owned by any user), or the
+   * password provided is incorrect.
    * @throws {HttpErrorInfo} Error 500 if an unexpected error occurs.
    */
   protected override async handleLogic(
@@ -54,9 +106,12 @@ export default class DeleteUserHandler extends Handler {
     next: express.NextFunction,
     client: DatabaseClient,
   ): Promise<void> {
+    const password: Password = DeleteUserHandler._parseQuery(req.query);
     const sessionToken: SessionToken = DeleteUserHandler._parseCookie(
       req.cookies,
     );
+
+    await DeleteUserHandler._validatePassword(client, sessionToken, password);
     await DeleteUserHandler._deleteUserProfile(client, sessionToken);
 
     res.sendStatus(200);
