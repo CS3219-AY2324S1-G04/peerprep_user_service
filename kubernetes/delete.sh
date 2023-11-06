9dca57fc6392480d0faa559fa288cd09389c6239
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-11-06 22:17:01 +0800

    Add kubernetes delete script

---
+++ kubernetes/delete.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+kubectl delete -f ./config_maps/core.yaml
+kubectl delete -f ./config_maps/database_client.yaml
+kubectl delete -f ./secrets/database_client.yaml
+
+kubectl delete -f ./config_maps/database_initialiser.yaml
+kubectl delete -f ./secrets/database_initialiser.yaml
+kubectl delete -f ./jobs/database_initialiser.yaml
+
+kubectl delete -f ./config_maps/api.yaml
+kubectl delete -f ./secrets/api.yaml
+kubectl delete -f ./deployments/api.yaml
+kubectl delete -f ./services/api.yaml
+kubectl delete -f ./hpas/api.yaml
