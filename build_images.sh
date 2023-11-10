9677f783ae892c4d86775ecd542a180be0e8afea
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-11-10 08:33:08 +0800

    Update build script to include image instructions

--- build_images.sh
+++ build_images.sh
@@ -4,14 +4,23 @@
 cr="ghcr.io/cs3219-ay2324s1-g04/"
 export_dir="./docker_build"
 
+################################################################################
+#                                                                              #
+#                  This marks the start of image information.                  #
+#                                                                              #
+#                        If you are adding a new image:                        #
+#   1. Add a unique key in "image_keys" for your image.                        #
+#   2. For each image specify the following variables:                         #
+#     - images_KEY_name - Name of the image. (should be prepended with the     #
+#       container registry ${cr})                                              #
+#     - images_KEY_docker_file - Path to the dockerfile.                       #
+#     - images_KEY_should_build - Should be 0. This value is set by CLI        #
+#       arguments.                                                             #
+#                                                                              #
+################################################################################
+
 # This list should contain a unique key for each image.
 image_keys=(api database_initialiser)
-
-# For each image specify the following variables:
-# - images_KEY_name - Name of the image. (should be prepended with the container registry ${cr})
-# - images_KEY_docker_file - Path to the dockerfile.
-# - images_KEY_should_build - Should be 0. This value is set by CLI arguments.
-# Note: KEY refers to a key in image_keys
 
 # API image
 images_api_name=${cr}peerprep_user_service_api
@@ -22,6 +31,12 @@ images_api_should_build=0
 images_database_initialiser_name=${cr}peerprep_user_service_database_initialiser
 images_database_initialiser_docker_file="./dockerfiles/database_initialiser.dockerfile"
 images_database_initialiser_should_build=0
+
+################################################################################
+#                                                                              #
+#                   This marks the end of image information.                   #
+#                                                                              #
+################################################################################
 
 # Instructions
 image_keys_str=""
